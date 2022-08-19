c5a06875c635776b68a8227ba1a80f6e30abb785
granantuin <49106818+granantuin@users.noreply.github.com>
2022-08-19 09:23:48 +0200

    Update enm_udra_st.py

--- enm_udra_st.py
+++ enm_udra_st.py
@@ -9,11 +9,10 @@ from st_aggrid import AgGrid
 
 st.set_page_config(page_title="ENM Platforma tres",layout="wide")
 
- 
-
 #load algorithm file gust
 algo_g_d0 = pickle.load(open("algorithms/gust_UDR_d0.al","rb"))
 algo_g_d1 = pickle.load(open("algorithms/gust_UDR_d1.al","rb"))
+algo_g_d2 = pickle.load(open("algorithms/gust_UDR_d2.al","rb"))
 
 #load raw meteorological model and get model variables
 meteo_model = get_meteogalicia_model_1Km(algo_g_d0["coor"])
@@ -27,34 +26,39 @@ if st.checkbox("¿Dibujar mapa con los puntos del modelo 1Km?"):
   st.plotly_chart(dist_map)
 
 #Select meteorological model wind features
-w_g0=(meteo_model[0:48].wind_gust0*1.94384).round(0).to_numpy()
-dir0=(meteo_model[0:48].dir0).round(0).to_numpy()
+w_g0=(meteo_model[0:72].wind_gust0*1.94384).round(0).to_numpy()
+dir0=(meteo_model[0:72].dir0).round(0).to_numpy()
 
 #select x _var
 model_x_var_g_d0 = meteo_model[:24][algo_g_d0["x_var"]]
 model_x_var_g_d1 = meteo_model[24:48][algo_g_d1["x_var"]]
+model_x_var_g_d2 = meteo_model[48:72][algo_g_d2["x_var"]]
 
 #forecast machine learning  gust knots
 gust_ml_d0 = (algo_g_d0["pipe"].predict(model_x_var_g_d0)*1.94384).round(0)
 gust_ml_d1 = (algo_g_d1["pipe"].predict(model_x_var_g_d1)*1.94384).round(0)
+gust_ml_d2 = (algo_g_d2["pipe"].predict(model_x_var_g_d2)*1.94384).round(0)
 
 #load algorithm file dir
 algo_dir_d0 = pickle.load(open("algorithms/dir_UDR_d0.al","rb"))
 algo_dir_d1 = pickle.load(open("algorithms/dir_UDR_d1.al","rb"))
+algo_dir_d2 = pickle.load(open("algorithms/dir_UDR_d2.al","rb"))
 
 #select x _var
 model_x_var_dir_d0 = meteo_model[:24][algo_dir_d0["x_var"]]
 model_x_var_dir_d1 = meteo_model[24:48][algo_dir_d1["x_var"]]
+model_x_var_dir_d2 = meteo_model[48:72][algo_dir_d2["x_var"]]
 
 #forecast machine learning wind direction degrees
 dir_ml_d0 = algo_dir_d0["pipe"].predict(model_x_var_dir_d0)
 dir_ml_d1 = algo_dir_d1["pipe"].predict(model_x_var_dir_d1)
+dir_ml_d2 = algo_dir_d2["pipe"].predict(model_x_var_dir_d2)
 
 #compare results
-df_show=pd.DataFrame({"Machine Learning dirección": np.concatenate((dir_ml_d0,dir_ml_d1),axis=0),
+df_show=pd.DataFrame({"Machine Learning dirección": np.concatenate((dir_ml_d0,dir_ml_d1,dir_ml_d2),axis=0),
                       "Modelo WRF dirección": dir0,
                       "Hora UTC":meteo_model[:48].index,
-                      "Machine Learning racha máxima": np.concatenate((gust_ml_d0,gust_ml_d1),axis=0),
+                      "Machine Learning racha máxima": np.concatenate((gust_ml_d0,gust_ml_d1,dir_ml_d2),axis=0),
                       "Modelo WRF racha máxima":w_g0,
                       })
                      
@@ -80,6 +84,7 @@ st.download_button(label="Descargar informe de calidad viento",
 #load algorithm file precipitation marin d0 d1
 algo_prec_d0=pickle.load(open("algorithms/prec_ENM_d0.al","rb"))
 algo_prec_d1=pickle.load(open("algorithms/prec_ENM_d1.al","rb"))
+algo_prec_d2=pickle.load(open("algorithms/prec_ENM_d2.al","rb"))
 
 #load raw meteorological model and get model variables
 meteo_model=get_meteogalicia_model_4Km(algo_prec_d1["coor"])
@@ -96,16 +101,18 @@ if st.checkbox("¿Dibujar mapa con los puntos del modelo 4 Km?"):
 #select x _var
 model_x_var_p0=meteo_model[:24][algo_prec_d0["x_var"]]
 model_x_var_p1=meteo_model[24:48][algo_prec_d1["x_var"]]
+model_x_var_p2=meteo_model[48:72][algo_prec_d2["x_var"]]
 
 #forecast machine learning precipitation
 prec_ml0=algo_prec_d0["pipe"].predict_proba(model_x_var_p0)
 prec_ml1=algo_prec_d1["pipe"].predict_proba(model_x_var_p1)
+prec_ml2=algo_prec_d2["pipe"].predict_proba(model_x_var_p2)
 
 #show results
-df_show_pre=pd.DataFrame(np.concatenate((prec_ml0,prec_ml1),axis=0),
+df_show_pre=pd.DataFrame(np.concatenate((prec_ml0,prec_ml1,prec_ml2),axis=0),
                          columns=["no p","Machine learning"])
-df_show_pre["Hora UTC"]=meteo_model.index[0:48]
-df_show_pre["Modelo WRF"]=np.around(meteo_model[:48].prec0.values,decimals=1)
+df_show_pre["Hora UTC"]=meteo_model.index[0:72]
+df_show_pre["Modelo WRF"]=np.around(meteo_model[:72].prec0.values,decimals=1)
 df_show_pre=df_show_pre.drop(columns=["no p"])
 df_show_pre['Machine learning'] = df_show_pre['Machine learning'].map("{:.0%}".format)
 st.title(""" Probabilidad de precipitación ENM con Modelo WRF y Machine Learning""")
