eba1d2bb22a472be39a418fc952d86298ea0507d
granantuin <49106818+granantuin@users.noreply.github.com>
2022-08-24 10:00:30 +0200

    Update enm_udra_st.py

--- enm_udra_st.py
+++ enm_udra_st.py
@@ -55,11 +55,11 @@ dir_ml_d1 = algo_dir_d1["pipe"].predict(model_x_var_dir_d1)
 dir_ml_d2 = algo_dir_d2["pipe"].predict(model_x_var_dir_d2)
 
 #compare results
-df_show=pd.DataFrame({"Machine Learning dirección": np.concatenate((dir_ml_d0,dir_ml_d1,dir_ml_d2),axis=0),
-                      "Modelo WRF dirección": dir0,
+df_show=pd.DataFrame({"Machine Learning dir": np.concatenate((dir_ml_d0,dir_ml_d1,dir_ml_d2),axis=0),
+                      "Modelo WRF dir": dir0,
                       "Hora UTC":meteo_model[:72].index,
-                      "Machine Learning racha máxima": np.concatenate((gust_ml_d0,gust_ml_d1,gust_ml_d2),axis=0),
-                      "Modelo WRF racha máxima":w_g0,
+                      "Machine Learning racha": np.concatenate((gust_ml_d0,gust_ml_d1,gust_ml_d2),axis=0),
+                      "Modelo WRF racha":w_g0,
                       })
                      
 st.title(""" Pronóstico viento en estación cabo Udra Modelo WRF de Meteogalicia y Machine Learning""")
