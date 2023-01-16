2ee9c749d8d29455863f1b53a73bd4054d13d049
granantuin <49106818+granantuin@users.noreply.github.com>
2023-01-16 11:20:04 +0100

    Update enm_udra_graf.py

--- enm_udra_graf.py
+++ enm_udra_graf.py
@@ -364,8 +364,7 @@ st.pyplot(fig)
 
 st.write("#### **Probabilidad de precipitación machine learning y precipitación observada**")
 fig, ax = plt.subplots(figsize=(10,8))
-df_final[["ML"]].dropna().map("{:.0f}%".format)[0:31].plot(ax=ax, grid=True, kind='bar')
-
+df_final["ML"].dropna().map("{:.0f}%".format)[0:31].plot(ax=ax, grid=True, kind='bar')
 st.pyplot(fig)
 
 #download  excel file  
